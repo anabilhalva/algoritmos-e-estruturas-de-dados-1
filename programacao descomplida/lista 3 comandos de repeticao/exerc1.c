58c7cf48f65d57fd3153b0a23c6a8d590b2028d5
Ana Bilhalva <anaalmeidabilhalva@gmail.com>
2025-10-23 19:18:09 -0300

    primeiro commit

---
+++ programacao descomplida/lista 3 comandos de repeticao/exerc1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int i;
+
+    printf("Os cincos primeiros multiplos de 3 sao:\n");
+    for(i = 1; i <= 5; i++) {
+        printf("%d\n", i * 3);
+    }
+    return 0;
+       
+}
